153789f1a76eed2ea1934051be54972a47489371
Carlos Santillán <carlosalberto.santillan@mail.polimi.it>
2025-08-29 19:25:41 +0200

    Added first files for filter

--- filters/filter.py
+++ filters/filter.py
@@ -1,9 +1,1 @@
-""" 
-Filter class, contains all the
- """
-from abc import ABC
-import functools 
-class Filter(ABC):
-    
-    def __init__(self, state, ):
-        
+# Maybe not necessary after all
