c4eb9d69ab744b7d79aea84a3641904d240a325f
Carlos Santillán <carlosalberto.santillan@mail.polimi.it>
2025-09-09 19:24:32 +0200

    Now can plot with CI

--- ekf_vindy/plotting/plotter.py
+++ ekf_vindy/plotting/plotter.py
@@ -1,10 +1,12 @@
 # TODO: Add the plot_phase functionality
 
+from matplotlib.colors import to_rgba
 from ekf_vindy.plotting import latex_available
 from typing import List
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import matplotlib.patches as mpatches
 
 def _generic_labels(components: int):
     """
@@ -12,15 +14,17 @@ def _generic_labels(components: int):
     """
     return [r"$x_{" + f"{i+1}" + r"}(t)$" for i in range(components)]
 
-def plot_trajectory(x: np.ndarray, time_instants: np.ndarray,  state_names: List[str] | None = None, legend_fontsize: int = 16, title: str = "", x_tick_skip: int = None
-                  , palette="muted", xlabel=r"$t$", ylabel=r"$y(t)$") \
-        -> tuple[plt.Figure, plt.Axes]:
+def plot_trajectory(x: np.ndarray, time_instants: np.ndarray, sdevs: np.ndarray | None = None,
+                    state_symbols: List[str] | None = None, state_names: List[str] | None = None, legend_fontsize: int = 16, title: str = "", x_tick_skip: int = None
+                  , palette="muted", xlabel=r"$t$", ylabel=r"$y(t)$"):
     
     """
     We assume that the x argument is of shape (T, n), where T are the time instances, and n is the number of dimensions.
     """
     # format title depending on LaTeX availability
     title_str = title if not latex_available else r"\textrm{" + title + "}"
+    # cred_str = " with 95\% Credible Intervals" if not latex_available else r" \textrm{with 95\% Credible Intervals}"
+
     state_dimension = x.shape[1] 
 
     # format labels based on LaTeX availability and handle the generic case
@@ -40,6 +44,14 @@ def plot_trajectory(x: np.ndarray, time_instants: np.ndarray,  state_names: List
     # plot each trajectory
     for i in range(state_dimension):
         ax.plot(time_instants, x[:, i], label=labels[i], lw=2, color=colors[i])
+        if sdevs is not None:
+            upper = x[:, i] + 1.96 * sdevs[:, i]
+            lower = x[:, i] - 1.96 * sdevs[:, i]
+            fill_color = to_rgba(colors[i], alpha=0.2)
+            ax.fill_between(time_instants, lower, upper, color=fill_color)
+
+            # if you wanna display multiple, this plotting always handled like this...
+            # ax.fill_between(time_instants, lower, upper, color=colors[i], alpha=0.15, hatch='//')
 
     # title and labels
     ax.set_title(title_str, fontsize=18, color='black', pad=25)
@@ -68,7 +80,10 @@ def plot_trajectory(x: np.ndarray, time_instants: np.ndarray,  state_names: List
     if state_dimension <= 6:
         ax.legend(frameon=True, fontsize=legend_fontsize, framealpha=1.0, 
                   edgecolor='black', fancybox=False)
-
+        
+    # if sdevs is not None:
+    #     patch = mpatches.Patch(color="gray", alpha=0.3, label=cred_str)
+    #     ax.legend(handles=ax.get_legend_handles_labels()[0] + [patch])
     # background color
     fig.patch.set_facecolor('white')
 
